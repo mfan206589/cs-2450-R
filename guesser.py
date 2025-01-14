2edfe4fbf747c312ff32666f52414dd3c947f09c
root <root@LAPTOP-7VKEEROM>
2025-01-14 15:25:35 -0700

    guesser.py

---
+++ guesser.py
@@ -0,0 +1,22 @@
+import random
+def age_gesser():
+    name = input("Please Enter Your Name: ")
+    print(f'Hello {name}, this program will guess your age please press Y or N if the guess is correct or wrong.')
+    User(name)
+def random_guess():
+    return random.randrange(15, 30)
+def User(name):
+    guess = random_guess()
+    print(f'Is this your age: {guess}')
+    user_input = input('Y/N: ')
+    while True:
+        if user_input == 'Y':
+            print(f'{name} is {guess} years old')
+            return False
+        elif user_input == 'N':
+            print('Rats!')
+            User(name)
+            return True
+        else:
+            return user_input
+age_gesser()
